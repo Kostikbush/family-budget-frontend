c6c601cab85ce351e56b1a667d68019a2aaedd1c
Konstantin <qwerty58649@yandex.ru>
2023-01-06 10:36:17 +0300

    improved code of Form

--- src/Components/Form/From.tsx
+++ src/Components/Form/From.tsx
@@ -32,77 +32,73 @@ export const Form = ({
   };
   return (
     <article className="form-container">
-      {vieEnterForm && (
-        <CSSTransition
-          timeout={500}
-          classNames="form-change"
-          in={vieEnterForm}
-          unmountOnExit
-        >
-          <div className="form-container__enter">
-            <h2>Вход</h2>
-            <div className="enter__inputs">
-              <InputForm
-                value={email}
-                placeholder="Введите email"
-                type="text"
-                setState={setEmail}
-              />
-              <InputForm
-                value={password}
-                placeholder="Введите пароль"
-                type="password"
-                setState={setPassword}
-              />
-            </div>
-            <div className="enter__btns">
-              <Btn text="Войти" handleClick={handleEnter} />
-              <Btn
-                handleClick={handleClickChangeForm}
-                children={<BsArrowRight className="arr-ml" />}
-                text="Регистрация"
-              />
-            </div>
+      <CSSTransition
+        timeout={500}
+        classNames="form-change"
+        in={vieEnterForm}
+        unmountOnExit
+      >
+        <div className="form-container__enter">
+          <h2>Вход</h2>
+          <div className="enter__inputs">
+            <InputForm
+              value={email}
+              placeholder="Введите email"
+              type="text"
+              setState={setEmail}
+            />
+            <InputForm
+              value={password}
+              placeholder="Введите пароль"
+              type="password"
+              setState={setPassword}
+            />
           </div>
-        </CSSTransition>
-      )}
-      {!vieEnterForm && (
-        <CSSTransition
-          timeout={500}
-          classNames="form-change"
-          in={!vieEnterForm}
-          unmountOnExit
-        >
-          <div className="form-container__enter">
-            <h2>Регистрация</h2>
-            <div className="registration__inputs">
-              <InputForm
-                placeholder="Введите имя"
-                type="text"
-                setState={setName}
-              />
-              <InputForm
-                placeholder="Введите email"
-                type="text"
-                setState={setEmail}
-              />
-              <InputForm
-                placeholder="Введите пароль"
-                type="password"
-                setState={setPassword}
-              />
-            </div>
-            <div className="enter__btns">
-              <Btn handleClick={handleRegistartion} text="Зарегистрироваться" />
-              <Btn
-                handleClick={handleClickChangeForm}
-                children={<BsArrowRight className="arr-ml" />}
-                text="Войти"
-              />
-            </div>
+          <div className="enter__btns">
+            <Btn text="Войти" handleClick={handleEnter} />
+            <Btn
+              handleClick={handleClickChangeForm}
+              children={<BsArrowRight className="arr-ml" />}
+              text="Регистрация"
+            />
           </div>
-        </CSSTransition>
-      )}
+        </div>
+      </CSSTransition>
+      <CSSTransition
+        timeout={500}
+        classNames="form-change"
+        in={!vieEnterForm}
+        unmountOnExit
+      >
+        <div className="form-container__enter">
+          <h2>Регистрация</h2>
+          <div className="registration__inputs">
+            <InputForm
+              placeholder="Введите имя"
+              type="text"
+              setState={setName}
+            />
+            <InputForm
+              placeholder="Введите email"
+              type="text"
+              setState={setEmail}
+            />
+            <InputForm
+              placeholder="Введите пароль"
+              type="password"
+              setState={setPassword}
+            />
+          </div>
+          <div className="enter__btns">
+            <Btn handleClick={handleRegistartion} text="Зарегистрироваться" />
+            <Btn
+              handleClick={handleClickChangeForm}
+              children={<BsArrowRight className="arr-ml" />}
+              text="Войти"
+            />
+          </div>
+        </div>
+      </CSSTransition>
     </article>
   );
 };
