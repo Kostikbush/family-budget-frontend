18046e24bf21923d4d919727af15899a54831c66
Konstantin <qwerty58649@yandex.ru>
2023-01-20 20:04:41 +0300

    implement login page tests

--- src/pages/HelloPage/HelloPage.tsx
+++ src/pages/HelloPage/HelloPage.tsx
@@ -1,7 +1,12 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import "./helloPage.scss";
 export const HelloPage = () => {
   const [isExitHelloPage, setIsExitHelloPage] = useState(false);
+  useMemo(() => {
+    setTimeout(() => {
+      setIsExitHelloPage(!isExitHelloPage);
+    }, 400000);
+  }, [isExitHelloPage]);
   return (
     <main className={isExitHelloPage ? "page-exit" : "page-enter"}>
       <section className="hello-page-wrapper">
