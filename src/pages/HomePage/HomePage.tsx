12facae30e9a439954fe3db718766dcacc1891a4
Konstantin <qwerty58649@yandex.ru>
2023-01-22 14:10:21 +0300

    Hello page dun

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+//import { useState } from "react";
 
 export const HomePage = () => {
-  const [isExitMainPage, setIsExitMainPage] = useState(false);
+  //const [isExitMainPage, setIsExitMainPage] = useState(false);
   return (
-    <main className={isExitMainPage ? "page-exit" : "page-enter"}>
+    <main className={"page-exit page-enter"}>
       <h1>Home Page</h1>
     </main>
   );
